1b209a3ed1eb9b42c9691fafc01093ef14fe2875
Mikuláš Poul <mikulaspoul@gmail.com>
2021-03-08 14:19:54 +0100

    Add a command to refresh all materialized views

---
+++ django_pgviews/management/commands/refresh_pgviews.py
@@ -0,0 +1,23 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from django_pgviews.models import ViewRefresher
+
+log = logging.getLogger("django_pgviews.refresh_pgviews")
+
+
+class Command(BaseCommand):
+    help = """Refresh materialized Postgres views for all installed apps."""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-C",
+            "--concurrently",
+            action="store_true",
+            dest="concurrently",
+            help="Refresh concurrently if the materialized view supports it",
+        )
+
+    def handle(self, concurrently, **options):
+        ViewRefresher().run(concurrently)
