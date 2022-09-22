d4c31466000d6196d4bd7bfe9c0fe74ed3482beb
Mikuláš Poul <mikulaspoul@gmail.com>
2022-09-22 10:29:06 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
     ],
 )
