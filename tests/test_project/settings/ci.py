236ad919e877aa422dadca4dcc8164839e0fa734
Mikuláš Poul <mikulaspoul@gmail.com>
2024-05-30 22:38:40 +0100

    Add third database with a set schema

--- tests/test_project/settings/ci.py
+++ tests/test_project/settings/ci.py
@@ -19,4 +19,13 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "5432",
     },
+    "schema_db": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME_WEATHER", "schemadb"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": "localhost",
+        "PORT": "5432",
+        "OPTIONS": {"options": "-c search_path=other"},
+    },
 }
